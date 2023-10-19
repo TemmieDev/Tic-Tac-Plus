22448b9c5c08b2f8e91beb0c46b9424ccdce3f64
Temmie <24ab@student.nbcs.nsw.edu.au>
2023-10-19 12:31:33 +1100

    Completely remade in-game UI inspired by persona 4

--- Tic Tac Toe/Scripts/main.gd
+++ Tic Tac Toe/Scripts/main.gd
@@ -17,23 +17,29 @@ var col_sum : int
 var diagonal1_sum : int
 var diagonal2_sum : int
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	board_size = $Board.texture.get_width()
+	# divide board size by 3 to get the size of individual cell
 	cell_size = board_size / 3
 	#get coordinates of small panel on right side of window
 	player_panel_pos = $PlayerPanel.get_position()
 	new_game()
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			#check if mouse is on the game board
-			if event.position.x < board_size and event.position.y < board_size + 240 and event.position.y > 240:
+			if event.position.x < board_size:
 				#convert mouse position into grid location
 				grid_pos = Vector2i(event.position / cell_size)
-				if grid_data[grid_pos.y - 1][grid_pos.x] == 0:
+				if grid_data[grid_pos.y][grid_pos.x] == 0:
 					moves += 1
-					grid_data[grid_pos.y - 1][grid_pos.x] = player
+					grid_data[grid_pos.y][grid_pos.x] = player
 					#place that player's marker
 					create_marker(player, grid_pos * cell_size + Vector2i(cell_size / 2, cell_size / 2))
 					if check_win() != 0:
@@ -41,19 +47,18 @@ func _input(event):
 						$GameOverMenu.show()
 						if winner == 1:
 							$GameOverMenu.get_node("ResultLabel").text = "Player 1 Wins!"
-						if winner == -1:
+						elif winner == -1:
 							$GameOverMenu.get_node("ResultLabel").text = "Player 2 Wins!"
-					#check is the board has been filled
+					#check if the board has been filled
 					elif moves == 9:
 						get_tree().paused = true
 						$GameOverMenu.show()
-						$GameOverMenu.get_node("ResultLabel").text = "Tie!"
+						$GameOverMenu.get_node("ResultLabel").text = "It's a Tie!"
 					player *= -1
 					#update the panel marker
 					temp_marker.queue_free()
-					create_marker(player, player_panel_pos + Vector2i(cell_size / 2, cell_size/ 2), true)
+					create_marker(player, player_panel_pos + Vector2i(cell_size / 2.07, cell_size / 2.1), true)
 					print(grid_data)
-
 
 func new_game():
 	player = 1
@@ -72,7 +77,7 @@ func new_game():
 	get_tree().call_group("circles", "queue_free")
 	get_tree().call_group("crosses", "queue_free")
 	#create a marker to show starting player's turn
-	create_marker(player, player_panel_pos + Vector2i(cell_size / 2, cell_size/ 2), true)
+	create_marker(player, player_panel_pos + Vector2i(cell_size / 2.07, cell_size / 2.1), true)
 	$GameOverMenu.hide()
 	get_tree().paused = false
 
@@ -83,20 +88,21 @@ func create_marker(player, position, temp=false):
 		circle.position = position
 		add_child(circle)
 		if temp: temp_marker = circle
-	if player == 1:
+	else:
 		var cross = cross_scene.instantiate()
 		cross.position = position
 		add_child(cross)
 		if temp: temp_marker = cross
 
 func check_win():
-	#add up markers in each row. column and diagonal
+	#add up the markers in each ros, column and diagonal
 	for i in len(grid_data):
-		row_sum = grid_data[i][0]+grid_data[i][1]+grid_data[i][2]
-		col_sum = grid_data[0][i]+grid_data[1][i]+grid_data[2][i]
-		diagonal1_sum = grid_data[0][0]+grid_data[1][1]+grid_data[2][2]
-		diagonal2_sum = grid_data[0][2]+grid_data[1][1]+grid_data[2][0]
-		#check if either player has all ofthe markers in one line
+		row_sum = grid_data[i][0] + grid_data[i][1] + grid_data[i][2]
+		col_sum = grid_data[0][i] + grid_data[1][i] + grid_data[2][i]
+		diagonal1_sum = grid_data[0][0] + grid_data[1][1] + grid_data[2][2]
+		diagonal2_sum = grid_data[0][2] + grid_data[1][1] + grid_data[2][0]
+	
+		#check if either player has all of the markers in one line
 		if row_sum == 3 or col_sum == 3 or diagonal1_sum == 3 or diagonal2_sum == 3:
 			winner = 1
 		elif row_sum == -3 or col_sum == -3 or diagonal1_sum == -3 or diagonal2_sum == -3:
